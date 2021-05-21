ef1482d48e012265fd450b499b82a03e335e388e
Dipanshu Gupta <dipanshug124@gmail.com>
2021-05-21 20:59:17 +0530

    download endpoint

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static('public'))
+
 //Template engine
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
@@ -15,6 +17,7 @@ connectDB();
 //Routes
 app.use('/api/files', require('./routes/files'));
 app.use('/files', require('./routes/show'));
+app.use('/files/download', require('./routes/download'))
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);

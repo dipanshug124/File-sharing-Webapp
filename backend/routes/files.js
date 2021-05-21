b9309bdf6dcd5d8324e5b281197a3ce0363db324
Dipanshu Gupta <dipanshug124@gmail.com>
2021-05-21 20:07:18 +0530

    completing

--- backend/routes/files.js
+++ backend/routes/files.js
@@ -21,13 +21,16 @@ let upload = multer({
 
 
 router.post('/', (req, res) => {
+    // Store file
     upload(req, res, async (err) => {
+        // Validate request
         if (!req.file) {
             return res.json({ error: 'All fields are required' });
         }
         if (err) {
             return res.status(500).send({ error: err.message });
         }
+        // Store into Database
         const file = new File({
             filename: req.file.filename,
             uuid: uuidv4(),

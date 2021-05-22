414a7beaecbe92a579cf39dfc79792032b265724
Dipanshu Gupta <dipanshug124@gmail.com>
2021-05-22 14:29:55 +0530

    all files

--- backend/routes/files.js
+++ backend/routes/files.js
@@ -42,4 +42,37 @@ router.post('/', (req, res) => {
     });
 });
 
+router.post('/send', async (req, res) => {
+    const { uuid, emailTo, emailFrom } = req.body;
+    // Validate request
+    if (!uuid || !emailTo || !emailFrom) {
+        return res.status(422).send({ error: 'All fields are required.' });
+    }
+
+    // Get data from database
+    const file = await File.findOne({ uuid: uuid });
+    if (file.sender) {
+        return res.status(422).send({ error: 'Email already sent.' });
+    }
+    file.sender = emailFrom;
+    file.receiver = emailTo;
+    const respose = await file.save();
+
+    // Sent email
+    const sentMail = require('../services/emailService')
+    sentMail({
+        from: emailFrom,
+        to: emailTo,
+        subject: 'Forkit file sharing',
+        text: `${emailFrom} shared a file with you`,
+        html: require('../services/emailTemplate')({
+            emailFrom: emailFrom,
+            downloadLink: `${process.env.APP_BASE_URL}/files/${file.uuid}`,
+            size: parseInt(file.size / 1000) + ' KB',
+            expires: '24 hours'
+        })
+    })
+    return res.send({ success: true })
+})
+
 module.exports = router;

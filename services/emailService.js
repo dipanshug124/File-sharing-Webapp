116c7355de600f1d7c5b205362ccc235dc2f1386
Dipanshu Gupta <dipanshug124@gmail.com>
2021-09-06 22:17:48 +0530

    smtp server changed

--- services/emailService.js
+++ services/emailService.js
@@ -2,15 +2,21 @@ require('dotenv').config();
 const nodemailer = require('nodemailer')
 async function sentMail({ from, to, subject, text, html }) {
     let transporter = nodemailer.createTransport({
-        service: 'gmail',
+        service: 'SendPulse', // no need to set host or port etc.
         auth: {
-            type: 'OAuth2',
-            user: process.env.MAIL_USER,
-            pass: process.env.MAIL_PASSWORD,
-            clientId: process.env.CLIENT_ID,
-            clientSecret: process.env.CLIENT_SECRET,
-            refreshToken: process.env.REFRESH_TOKEN
+            user: 'account.email@example.com',
+            pass: 'smtp-password'
         }
+
+        // service: 'gmail',
+        // auth: {
+        //     type: 'OAuth2',
+        //     user: process.env.MAIL_USER,
+        //     pass: process.env.MAIL_PASSWORD,
+        //     clientId: process.env.CLIENT_ID,
+        //     clientSecret: process.env.CLIENT_SECRET,
+        //     refreshToken: process.env.REFRESH_TOKEN
+        // }
     });
 
     let info = await transporter.sendMail({
